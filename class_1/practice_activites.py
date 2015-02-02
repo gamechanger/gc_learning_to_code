4394d2983e0d88e1177319460797277079fcc1d6
Eduardo Arenas <eduardo.arenas@gamechanger.io>
2015-02-01 19:45:33 -0500

    Increase spacing between sections and print activity headers so it's easier to read and follow

--- class_1/practice_activites.py
+++ class_1/practice_activites.py
@@ -1,8 +1,15 @@
-# The point of this file is to get you to start working with the different types we covered today
+# Instructions: The point of this file is to get you to start working with the different types we covered today. 
+
+
+
 # Section 1: Ints
 
 # Activity 1
-'''x = 2
+print("--------------------------------------------------")
+print("Activity 1")
+print("--------------------------------------------------")
+
+x = 2
 y = 5
 print(x+y)
 print(x-y)
@@ -12,31 +19,48 @@ print(y/x)
 print(x%y)
 print(y%x)
 print(x**y)
-print(y**x)'''
+print(y**x)
 
 # What are some int values for x and y, which cause the following statement to be true? (Feel free to do use the REPL to try out it out)
-'''print x % y == 5'''
+print x % y == 5
 
 # Activity 2
 # What int values for x and y will make the following statements true?
-'''print x / y * y == x
-print x / y == .5'''
+print("--------------------------------------------------")
+print("Activity 2")
+print("--------------------------------------------------")
+
+print x / y * y == x
+print x / y == .5
 
 # Activity 3
-'''print abs(-5)
+print("--------------------------------------------------")
+print("Activity 3")
+print("--------------------------------------------------")
+
+print abs(-5)
 print max(2,3)
 print min(2,3)
-print pow(2,3)'''
+print pow(2,3)
 
 # What int values of x and y will make the following statements true?
-'''print min(max(x,5), max(y, 3)) == 4
+print min(max(x,5), max(y, 3)) == 4
 print min(max(x,5), max(y, 3)) == 10
-print min(max(x,5), max(y, 3)) == 0'''
+print min(max(x,5), max(y, 3)) == 0
+
+
+
+
 
 
 # Section 2: floats
+
 # Activity 4
-'''x = 2.0
+print("--------------------------------------------------")
+print("Activity 4")
+print("--------------------------------------------------")
+
+x = 2.0
 y = 5.0
 print(x+y)
 print(x-y)
@@ -44,15 +68,24 @@ print(x*y)
 print(x/y)
 print(y/x)
 print(x**y)
-print(y**x)'''
+print(y**x)
 
 # What value for x makes these statements true?
-'''print x / y * y == x
-print x / y == .5'''
+print x / y * y == x
+print x / y == .5
+
+
+
+
 
 # Section 3: bools
+
 # Activity 5
-'''print (not False)
+print("--------------------------------------------------")
+print("Activity 5")
+print("--------------------------------------------------")
+
+print (not False)
 print (not True)
 print (False or False)
 print (False or True)
@@ -62,22 +95,35 @@ print (False and False)
 print (False and True)
 print (True and False)
 print (True and True)
-'''
+
 # What value do each of the following statements return?
-'''(True and False) or (True or False)
-(not False or False) and not (False and True)'''
+(True and False) or (True or False)
+(not False or False) and not (False and True)
 
 # Activity 6
 # What values for x and y make the following statements true?
-'''print (x and x) == True
+print("--------------------------------------------------")
+print("Activity 6")
+print("--------------------------------------------------")
+
+print (x and x) == True
 print (x or x) == True
 print (x and x) == x
 print (x or x) == x
-print (x and y or not x) == True'''
+print (x and y or not x) == True
+
+
+
+
 
 # Section 4: strings
+
 # Activity 7
-'''x = "hello"
+print("--------------------------------------------------")
+print("Activity 7")
+print("--------------------------------------------------")
+
+x = "hello"
 y = "world"
 print x
 print y
@@ -88,21 +134,29 @@ print x + str(5) + y
 print x + 5 + y
 print len(x) + len(y)
 print x == "hello"
-print y == "WORLD"'''
+print y == "WORLD"
 
 # Using variables x and y write a statement that will print out "Coding is easy"
-'''x = "oding"
-y = "is"'''
+x = "oding"
+y = "is"
 
 # Activity 8
+print("--------------------------------------------------")
+print("Activity 8")
+print("--------------------------------------------------")
+
 # Using variable x and only addition and subtraction make the following statements true
-'''x = "Fun"
+x = "Fun"
 print x == "Fun, Fun, Fun, Fun, Fun, "
-print x == "Fun, Fun, Fun, Fun, Fun"'''
+print x == "Fun, Fun, Fun, Fun, Fun"
 
 
 # Activity 9
+print("--------------------------------------------------")
+print("Activity 9")
+print("--------------------------------------------------")
+
 # What values of x so that the following statements are true?
-'''x = 1
+x = 1
 print (len("GameChanger") / 5) * x == 100
-print len("GameChanger") / x == .5'''
+print len("GameChanger") / x == .5
