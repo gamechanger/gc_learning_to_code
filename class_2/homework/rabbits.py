36e1c83a9e7fb6a3d788310f99bcb3d5017ae431
Gabriel Khaselev <gabriel.khaselev@gamechanger.io>
2015-02-11 20:04:45 -0500

    new hmwrk

--- class_2/homework/rabbits.py
+++ class_2/homework/rabbits.py
@@ -2,6 +2,12 @@
 
 def main():
 	"""
+	YOU CAN RUN THIS PROGRAM BY TYPING:
+
+		python rabbits.py 
+
+	ON THE COMMAND LINE IN THE SAME DIRECTORY THAT THIS FILE IS IN.
+
 	You discover a pair of Rabbits inside the Board Room. Assume they breed as follows:
 
 		1. Each year, each rabbit mates with exactly one other rabbit, with each pair of rabbits producing a litter of 4 bunnies. 
@@ -9,12 +15,16 @@ def main():
 
 	Based on these numbers, write a program to show how the Board Room rabbit population will change over the next 20 years. Your program should output the rabbit population every year for 20 years. You can format the output any way you like, but it should include some introductory text that explains what the program is doing, and it should clearly present the output data.	
 	To help you check your program, the population in 2015 is 6 rabbits (2 rabbits from 2014 plus 4 bunnies, with no adoptions), the population in 2016 is 18 rabbits (6 from 2015 plus 12 bunnies, with no adoptions), the population in 2017 is 53 (18 rabbits from the previous year, 36 bunnies from the 9 rabbit pairs, and 1 adoption), and the population in 2018 is 152 (53 rabbits from the previous year, 104 bunnies from the 26 full rabbit pairs, and 5 adoptions).
+	
 	"""
 
 	# BTW that long tripple quoted section is called a Doc String.
 	# They are commonly used to explain what a program or function is supposed to do.
 
-
+	# Hint 1: Use variables to save the current state of the rabbit colony
+	#		  i.e. store the current population, number of first year rabbits (etc.)
+	# Hint 2: Use a while loop to make sure you calculate population changes
+	#   	  for 20 years!
 
 
 
---
+++ class_2/homework/text_adventure.py
@@ -0,0 +1,40 @@
+
+def main():
+	"""
+	YOU CAN RUN THIS PROGRAM BY TYPING:
+
+		python text_adventure.py 
+
+	ON THE COMMAND LINE IN THE SAME DIRECTORY THAT THIS FILE IS IN.
+	
+	In This assignment you will be creating a text based adventure game
+	similar to the demo.py file in this repository.
+	Your adventure can be about anything! Make it a fun adventure for your
+	friends to play through!
+
+	RULES:
+
+	1. 	There must be at least 16 questions in your game (total).
+	2. 	Each question must have 2-4 possible choices
+	3. 	You must use if/elif/else statements!
+	4. 	At the end of the adventure you must ask the user if they want to play
+		the game again. If they do play again! (Hint: What can we use if we want
+		to continue forever unless we meet a certain condition?)
+	5. 	You can't let the user enter anything they want when you ask a question.
+		Make sure they only input answers that make sense!
+
+	Play through the demo game to get a feel for how the game should work.
+	If you copy and paste my code I will know...And I will sue your ass for 
+	copy write infringement!
+	
+	"""
+
+
+
+
+
+
+
+
+# DO NOT REMOVE THIS LINE!	
+main()
