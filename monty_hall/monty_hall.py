4a4ccbea3fe989d25252380516afc08f0a9daf70
Erik Taubeneck <erik.taubeneck@gmail.com>
2015-02-25 10:19:35 -0500

    monty-hall-updates: update monty hall to code written in class, add marbles.py code

--- monty_hall/monty_hall.py
+++ monty_hall/monty_hall.py
@@ -1,31 +1,37 @@
 import random
 
-SWITCH = True
-goats = 0
+switch = False
 boats = 0
+goats = 0
 runs = 1000000
 
 for run in range(runs):
-    doors = ['boat', 'goat', 'goat']
+    doors = ['goat', 'goat', 'boat']
 
     random.shuffle(doors)
 
-    selection = doors.pop(random.randint(0, 2))
+    choice = random.randint(0, 2)
 
-    if selection == 'boat':
-        door_to_show = doors.pop(random.randint(0, 1))
-    elif selection == 'goat':
-        door_to_show = doors.remove('goat')
+    selection_door = doors.pop(choice)
+
+    if selection_door == 'boat':
+        # we got a boat!
+        # doors = ['goat', goat']
+        goat_door = doors.pop(random.randint(0, 1))
+    elif selection_door == 'goat':
+        # we got a goat
+        # doors = ['goat', 'boat']
+        # doors = ['boat', goat']
+        goat_door = doors.remove('goat')
 
     remaining_door = doors[0]
 
-    if SWITCH:
-        selection, remaining_door = remaining_door, selection
-
-    if selection == 'boat':
-        boats += 1
-    elif selection == 'goat':
+    if switch:
+        selection_door, remaining_door = remaining_door, selection_door
+    if selection_door == 'goat':
         goats += 1
+    elif selection_door == 'boat':
+        boats += 1
 
 print 'boats: {}%, goats: {}%'.format(
     100*boats/float(runs), 100*goats/float(runs))
