1de09c2a0b4b6d81470379416cd41176f5601e1a
byels <byelsey1@gmail.com>
2015-03-02 15:25:51 -0500

    final updates to class_3 Keynote

---
+++ class_3/list_docs.py
@@ -0,0 +1,24 @@
+# list methods:
+
+list = [1, 3, 5, 7, 9, 11, 13]
+
+    list.append
+    list.append("cheese")
+    -> [1, 3, 5, 7, 9, 11, 13, "cheese"]
+    
+    list.insert
+    list.insert(2, "cheese")
+    -> [1, 3, "cheese", 5, 7, 9, 11, 13]
+    
+    list.pop()
+    -> [1, 3, 5, 7, 9, 11]
+
+    list.pop(0)
+    -> [3, 5, 7, 9, 11, 13]
+
+    
+    list.reverse()
+    -> [13, 11, 9, 7, 5, 3, 1]
+    
+    list.sort()
+    
