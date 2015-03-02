e6d6587aca95f33f0a73a6b6664bb94bffd0e4fb
byels <byelsey1@gmail.com>
2015-03-02 15:31:38 -0500

    updates to list docs

--- class_3/list_docs.py
+++ class_3/list_docs.py
@@ -1,24 +1,27 @@
 # list methods:
 
-list = [1, 3, 5, 7, 9, 11, 13]
+list = [3, 1, 5, 7, 9, 11, 13]
 
-    list.append
-    list.append("cheese")
-    -> [1, 3, 5, 7, 9, 11, 13, "cheese"]
+# list.append
+list.append("cheese")
+ -> [3, 1, 5, 7, 9, 11, 13, "cheese"]
     
-    list.insert
-    list.insert(2, "cheese")
-    -> [1, 3, "cheese", 5, 7, 9, 11, 13]
-    
-    list.pop()
-    -> [1, 3, 5, 7, 9, 11]
+# list.insert
+list.insert(2, "cheese")
+ -> [3, 1, "cheese", 5, 7, 9, 11, 13]
 
-    list.pop(0)
-    -> [3, 5, 7, 9, 11, 13]
+# list.pop
+list.pop()
+ -> [3, 1, 5, 7, 9, 11]
 
-    
-    list.reverse()
-    -> [13, 11, 9, 7, 5, 3, 1]
-    
-    list.sort()
+list.pop(0)
+ -> [1, 5, 7, 9, 11, 13]
+
+# list.reverse
+list.reverse()
+ -> [13, 11, 9, 7, 5, 1, 3]
+
+# list.sort
+list.sort()
+ -> [1, 3, 5, 7, 9, 11, 13]
     
